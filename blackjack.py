2b19ba8d51e11d1843422fc23069118cb46e58a7
03_saint <misaelruizgz@outlook.com>
2025-11-13 14:45:02 -0700

    idk

--- blackjack.py
+++ blackjack.py
@@ -45,8 +45,10 @@ class Deck:
     cards = ['2','3','4','5','6','7','8','9','10','J','Q','K','A'] 
     suits = ['♠︎' , '♥︎' , '♦︎' , '♣︎']
 
+    mixed_deck = {}
+
     for card in cards:
-        mixed_deck = []
+        
 
 
 
